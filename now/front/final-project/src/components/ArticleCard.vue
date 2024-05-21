2da1836524e9e267cc42f02f13e1fbec391e3af9
chungwon <yud01063@gmail.com>
2024-05-21 10:05:47 +0900

    comment_update

--- now/front/final-project/src/components/ArticleCard.vue
+++ now/front/final-project/src/components/ArticleCard.vue
@@ -2,17 +2,18 @@
   <div class="d-flex justify-content-center" @click = "goDetail(movie.id)">
 
     <div class="d-flex align-items-center justify-content-between">
-      <div class="ticket d-flex align-items-center justify-content-between">
+      <div class="ticket d-flex align-items-center" style="width: 1200px">
         <img class="img" :src="`https://image.tmdb.org/t/p/original/${prop.movie.poster_path}`" alt="X">
-        <div>
+        <div class="col-md-6 offset-md-1" style="margin: 0; width:750px">
           <h2>영화 관람권</h2>
           <p>{{ movie.title }}</p>
           <p>{{ movie.released_date }}</p>
         </div>
-      </div>
-      <div class="borderst">
-        <div class="barcode">
-          바코드
+        <!-- <div class="barcode-container justify-content-center align-items-center" >
+          <img :src="qrcode" alt="X" style="max-width: 100%; height: 40%; display: block; margin: auto;">
+        </div> -->
+        <div class="barcode-container col-md-3 offset-md-10" style="margin: 0; width:200px;">
+          <img :src="barcode" class="d-block" alt="X" style="margin: 0; width: 100%;">
         </div>
       </div>
     </div>
@@ -20,8 +21,10 @@
 </template>
 
 <script setup>
-import { useRouter } from 'vue-router';
+import { useRouter } from 'vue-router'
 import { ref } from 'vue'
+import barcode from '@/assets/Barcode.jpg'
+import qrcode from '@/assets/QRcode.png'
 const prop = defineProps({
   movie:Object
 })
@@ -52,11 +55,15 @@ function goDetail(movieId){
   height: 18rem;
 }
 
-.barcode{
+.barcode-container {
   /* background-color: green; */
-  margin: 1rem;
+  margin: 0;
   width : 90%;
   height: 90%;
+  background-color: lightgoldenrodyellow;
+  display: flex;
+  justify-content: center;
+  text-align: center;
 }
 
 .borderst{
@@ -65,7 +72,7 @@ function goDetail(movieId){
   justify-content: center;
   width: 15vw;
   height: 20rem;
- 
   border : 2px solid white; 
+  text-align: center;
 }
 </style>
